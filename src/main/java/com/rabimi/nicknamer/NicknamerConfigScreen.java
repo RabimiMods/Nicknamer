b0d605a7626de08e267fc229cc3dc526a53b431c
RabimiYT <fakerabimi@gmail.com>
2025-12-10 10:37:53 +0900

    Implement Nicknamer configuration screen

---
+++ src/main/java/com/rabimi/nicknamer/NicknamerConfigScreen.java
@@ -0,0 +1,35 @@
+package com.rabimi.nicknamer;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.text.Text;
+
+public class NicknamerConfigScreen extends Screen {
+
+    private TextFieldWidget field;
+
+    public NicknamerConfigScreen() {
+        super(Text.literal("Nicknamer Settings"));
+    }
+
+    @Override
+    protected void init() {
+        field = new TextFieldWidget(textRenderer, width / 2 - 100, height / 2 - 10, 200, 20, Text.literal("Nickname"));
+        field.setText(NicknamerClient.getNickname());
+        addSelectableChild(field);
+
+        addDrawableChild(ButtonWidget.builder(Text.literal("保存"), (btn) -> {
+            NicknamerClient.setNickname(field.getText());
+            close();
+        }).dimensions(width / 2 - 40, height / 2 + 20, 80, 20).build());
+    }
+
+    @Override
+    public void render(DrawContext ctx, int mouseX, int mouseY, float delta) {
+        renderBackground(ctx);
+        ctx.drawCenteredTextWithShadow(textRenderer, "ニックネーム設定", width / 2, height / 2 - 40, 0xFFFFFF);
+        super.render(ctx, mouseX, mouseY, delta);
+    }
+}
