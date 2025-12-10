008c5276c7bb1b72e637f899cbe4261a6a482982
RabimiYT <fakerabimi@gmail.com>
2025-12-10 11:32:08 +0900

    Modify PlayerListHud to change player display name

--- src/main/java/com/rabimi/nicknamer/mixin/PlayerListEntryMixin.java
+++ src/main/java/com/rabimi/nicknamer/mixin/PlayerListEntryMixin.java
@@ -1,6 +1,8 @@
 package com.rabimi.nicknamer.mixin;
 
 import com.rabimi.nicknamer.NicknamerClient;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,11 +10,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerListEntry.class)
-public class PlayerListEntryMixin {
+@Mixin(PlayerListHud.class)
+public class PlayerListHudMixin {
 
-    @Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
-    private void changeTabName(CallbackInfoReturnable<Text> cir) {
-        cir.setReturnValue(Text.literal(NicknamerClient.getNickname()));
+    @Inject(method = "getPlayerName", at = @At("HEAD"), cancellable = true)
+    private void onGetPlayerName(PlayerListEntry entry, CallbackInfoReturnable<Text> cir) {
+
+        var client = MinecraftClient.getInstance();
+        if (!entry.getProfile().getId().equals(client.getSession().getUuid())) return;
+
+        String nick = NicknamerClient.getNickname();
+        if (nick == null || nick.isEmpty()) return;
+
+        cir.setReturnValue(Text.literal(nick));
     }
 }
